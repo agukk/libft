cc518756ded9c1171f7465ff26d489b362449380
Kenta Kato <01keny01@gmail.com>
2024-04-28 09:36:17 +0900

    update

--- libft/srcs/ft_strlcpy.c
+++ libft/srcs/ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: kentakato <kentakato@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/27 12:37:44 by kentakato         #+#    #+#             */
-/*   Updated: 2024/04/27 19:59:08 by kentakato        ###   ########.fr       */
+/*   Updated: 2024/04/28 09:33:49 by kentakato        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,15 @@
 size_t ft_strlcpy(char *dst, const char *src, size_t dstsize)
 {
     size_t i = 0;
-    while (--dstsize)
+    size_t src_len = ft_strlen(src);
+    if (dstsize == 0)
+        return src_len;
+    while (i < dstsize - 1 && src[i] != '\0')
     {
         dst[i] = src[i];
+        i++;
     }
+    dst[i] = '\0';
+    
+    return src_len;
 }
