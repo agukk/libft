115c3dd254c9f9a22110e2e51d0fcd4ba4718369
Kenta Kato <01keny01@gmail.com>
2024-05-02 19:36:56 +0900

    update

--- libft/srcs/ft_strlcat.c
+++ libft/srcs/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: kentakato <kentakato@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/28 09:37:38 by kentakato         #+#    #+#             */
-/*   Updated: 2024/04/28 11:14:39 by kentakato        ###   ########.fr       */
+/*   Updated: 2024/04/29 14:25:22 by kentakato        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,13 @@
 size_t ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
     size_t i = 0;
-    size_t dst_len = ft_strlen(dst);
-    size_t src_len = ft_strlen(src);
 
     if (dst == NULL && dstsize == 0)
-        return src_len;   
-
+        return ft_strlen(src);   
+        
+    size_t dst_len = ft_strlen(dst);
+    size_t src_len = ft_strlen(src);
+    
     if (dstsize <= dst_len)
         return src_len + dstsize;
 
---
+++ libft/srcs/ft_strnstr.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kentakato <kentakato@student.42.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/28 19:40:58 by kentakato         #+#    #+#             */
+/*   Updated: 2024/05/02 12:17:44 by kentakato        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/libft.h"
+
+/* 部分文字列 needle が検索された len 文字より少ない文字列 haystack の中で最初に現れる位置を見つける
+ * ◾️ 引数
+ * #1．検索対象のメモリブロック
+ * #2．検索文字列
+ * #3．検索バイト数
+ * ◾️ 返り値
+ * ・一致した文字へのポインター（文字列が見つかった場合）
+ * ・NULL（文字列が見つからない場合）
+ * ◾️ point
+ * ・needle_lenよりもlenの方が小さいなら絶対に文字が一致することはないから、そこから条件を考える
+ */
+
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+{
+	size_t	needle_len;
+
+	if (haystack == NULL && len == 0)
+	{
+		return (NULL);
+	}
+	needle_len = ft_strlen(needle);
+	while (*haystack != '\0' && needle_len <= len)
+	{
+		if (ft_strncmp(haystack, needle, needle_len) == 0)
+		{
+			return ((char *)haystack);
+		}
+		haystack++;
+		len--;
+	}
+	return (NULL);
+}
