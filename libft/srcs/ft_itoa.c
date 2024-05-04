605c9c810bb775dc8adc67f877706fb19978c024
Kenta Kato <01keny01@gmail.com>
2024-05-04 19:46:37 +0900

    update

---
+++ libft/srcs/ft_itoa.c
@@ -0,0 +1,72 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kentakato <kentakato@student.42.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/04 18:43:41 by kentakato         #+#    #+#             */
+/*   Updated: 2024/05/04 19:45:59 by kentakato        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/libft.h"
+
+size_t	intlen(int n)
+{
+	size_t	len;
+	long	num;
+
+	len = 1;
+	num = n;
+	if (num < 0)
+	{
+		num *= -1;
+		len++;
+	}
+	while (num >= 10)
+	{
+		num /= 10;
+		len++;
+	}
+	return (len);
+}
+
+char	*ft_itoa(int n)
+{
+	char	*str;
+	long	num;
+	size_t	len;
+
+	len = intlen(n);
+	num = n;
+	if (num < 0)
+		num *= -1;
+	str = (char *)ft_calloc(len + 1, sizeof(char));
+	if (str == NULL)
+		return (NULL);
+	while (len > 0)
+	{
+		str[len - 1] = (num % 10) + '0';
+		num /= 10;
+		len--;
+	}
+	if (n < 0)
+		str[len] = '-';
+	return (str);
+}
+
+// #include <stdio.h>
+
+// int	main(void)
+// {
+// 	size_t res = intlen(-12);
+// 	printf("%zu\n", res);
+
+// 	char *res = ft_itoa(INT_MIN);
+// 	printf("INT_MIN : %s\n", res);
+// 	char *res1 = ft_itoa(-1);
+// 	printf("%s\n", res1);
+// 	char *res2 = ft_itoa(-100);
+// 	printf("%s\n", res2);
+// }
