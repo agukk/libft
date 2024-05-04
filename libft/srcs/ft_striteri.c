5d60e55f2782c3fcb3d5db1cca3aeb9c3bf7c19c
Kenta Kato <01keny01@gmail.com>
2024-05-04 22:11:24 +0900

    update

--- libft/srcs/ft_striteri.c
+++ libft/srcs/ft_striteri.c
@@ -1,42 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*   ft_striteri.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: kentakato <kentakato@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/05/04 20:20:44 by kentakato         #+#    #+#             */
-/*   Updated: 2024/05/04 22:04:32 by kentakato        ###   ########.fr       */
+/*   Created: 2024/05/04 21:13:57 by kentakato         #+#    #+#             */
+/*   Updated: 2024/05/04 22:08:21 by kentakato        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/libft.h"
 
-char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+void	ft_striteri(char *s, void (*f)(unsigned int, char *))
 {
-	unsigned int	i;
-	size_t			len;
-	char			*str;
+	size_t	i;
 
-	if (s == NULL || f == NULL)
-		return (NULL);
 	i = 0;
-	len = ft_strlen(s);
-	str = (char *)ft_calloc(len + 1, sizeof(char));
-	if (str == NULL)
-		return (NULL);
-	while (i < len)
+	if (s == NULL || f == NULL)
+		return ;
+	while (s[i] != '\0')
 	{
-		str[i] = f(i, s[i]);
+		f(i, &s[i]);
 		i++;
 	}
-	str[i] = '\0';
-	return (str);
 }
-
-// char	test(unsigned int i, char c)
-// {
-// 	i = 0;
-// 	c = 'A' - i;
-// 	return (c);
-// }
