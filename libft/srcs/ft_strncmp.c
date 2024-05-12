6882b75411ae8fc48f9012baa2e34bff90a2d0ef
Kenta Kato <01keny01@gmail.com>
2024-05-13 00:02:39 +0900

    fix

--- libft/srcs/ft_strncmp.c
+++ libft/srcs/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: kentakato <kentakato@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/28 18:11:16 by kentakato         #+#    #+#             */
-/*   Updated: 2024/05/12 23:06:51 by kentakato        ###   ########.fr       */
+/*   Updated: 2024/05/13 00:02:35 by kentakato        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	size_t	i;
 
 	i = 0;
-	if (s1 == NULL || s2 == NULL)
+	if ((s1 == NULL || s2 == NULL) && n == 0)
 		return (0);
 	while (s1[i] != '\0' && s2[i] != '\0' && i < n)
 	{
@@ -34,6 +34,9 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 
 // int main()
 // {
-// 	printf("ft : %d, lib : %d\n", ft_strncmp("hello", NULL, 0), strncmp("hello",
-			// NULL, 0));
+// 	printf("ft : %d, lib : %d\n", ft_strncmp("hello", NULL, 0), strncmp("hello", NULL, 0));
+// 	printf("ft : %d, lib : %d\n", ft_strncmp("nope", ((void *)0), 3), strncmp("nope", ((void *)0), 3));
+// 	printf("ft : %d\n", ft_strncmp("nope", ((void *)0), 3));
+// 	printf("lib : %d\n", strncmp("nope", ((void *)0), 3));
+// 	printf("ft : %d, lib : %d\n", ft_strncmp(((void *)0), ((void *)0), 3), strncmp(((void *)0), ((void *)0), 3));
 // }
